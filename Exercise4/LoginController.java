5b975633dccfa25ecc71be97a9458c3ad267cb25
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2023-05-18 12:05:39 +0800

    Add files via upload

---
+++ Exercise4/LoginController.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.util.EventObject;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    @FXML
+    TextField mytextfield;
+
+    @FXML
+    PasswordField mypasswordfield;
+
+    @FXML
+    Button mybutton;
+
+    @FXML
+    Label mywarninglabel;
+
+    public void login(ActionEvent event) throws IOException {
+
+        String username = mytextfield.getText();
+        String password = mypasswordfield.getText();
+
+        System.out.println(username);
+        System.out.println(password);
+
+        if (username.equals("smig") && password.equals("123456")) {
+
+            Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } else {
+            mywarninglabel.setVisible(true);
+
+        }
+
+    }
+
+}
