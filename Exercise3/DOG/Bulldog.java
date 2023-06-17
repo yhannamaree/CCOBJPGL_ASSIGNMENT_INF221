090bfefb2b1849ae3b5532163119aac8b33467e6
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2023-06-18 07:37:43 +0800

    Create Bulldog.java

---
+++ Exercise3/DOG/Bulldog.java
@@ -0,0 +1,9 @@
+class SiberianHusky implements Havanese {
+    @Override
+
+    public void bark() {
+        System.out.println(" ");
+        System.out.println("Basenji barks!");
+        System.out.println("I am a dramatic Bull dogs!");
+    }
+}
