d76957ca19de310c2e82303145906bba4b38558d
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2023-06-18 06:52:48 +0800

    Create Rottweiler.java

---
+++ Exercise3/DOG/Rottweiler.java
@@ -0,0 +1,12 @@
+package DOG;
+
+class Rottweiler implements Runnable {
+    @Override
+
+    public void run() {
+        System.out.println(" ");
+        System.out.println("Rottweiler runs!");
+        System.out.println("Rottweiler is on the way!");
+        System.out.println(" ");
+    }
+}
