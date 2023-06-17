c154213559a0f93e266e08e3dbdbb5ff938961b8
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2023-06-18 06:16:44 +0800

    Create Main.java

---
+++ Exercise3/DOG/Main.java
@@ -0,0 +1,9 @@
+package DOG;
+
+public class Main {
+    public static void main(String[] args) {
+        MixedBreedDog dog = new MixedBreedDog();
+        dog.bark();  // Output: "Basenji barks!"
+        dog.run();   // Output: "Rottweiler runs!"
+    }
+}
